08eb6ba041d4a8925c87ea9aa6ceefb411993374
Daniel Martín <daniel.martin@jobandtalent.com>
2016-01-25 09:24:17 +0100

    Fontify DONE tasks
    Strikethrough done tasks so that they can be easily recognized visually.

--- .emacs.d/src/org-mode-config.el
+++ .emacs.d/src/org-mode-config.el
@@ -1,3 +1,12 @@
+(setq org-fontify-done-headline t)
+(custom-set-faces
+ '(org-done ((t (:foreground "PaleGreen"
+                             :weight normal
+                             :strike-through t))))
+ '(Org-headline-done
+   ((((class color) (min-colors 16) (background dark))
+     (:foreground "LightSalmon" :strike-through t)))))
+
 (setq org-log-done 'time)
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
