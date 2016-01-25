576a99bdf883a28d01640a8ce26459efce9c8511
Daniel Martín <daniel.martin@jobandtalent.com>
2016-01-25 21:59:02 +0100

    Update Org-mode configuration

--- .emacs.d/src/org-mode-config.el
+++ .emacs.d/src/org-mode-config.el
@@ -1,3 +1,4 @@
+(setq org-directory "~/Org-Mode")
 (setq org-fontify-done-headline t)
 (custom-set-faces
  '(org-done ((t (:foreground "PaleGreen"
@@ -11,7 +12,15 @@
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-cL" 'org-insert-link-global)
+(define-key global-map "\C-cr" 'org-capture)
 (define-key global-map "\C-ca" 'org-agenda)
+
+;; Org-Mode Capture
+(setq org-default-notes-file "~/Org-Mode/organizer.org")
+
+;; Org-Mode Agenda
+(setq org-agenda-files '("~/Org-Mode/"))
 
 (global-font-lock-mode 1)		     ; for all buffers
 (add-hook 'org-mode-hook 'turn-on-font-lock)  ; org-mode buffers only
