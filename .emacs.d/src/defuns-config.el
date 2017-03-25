c48afc2c9ad6989cfcdcae8a545ca9b42bae5b6e
Daniel Martín <mardani29@yahoo.es>
2017-03-25 19:45:09 +0100

    Make C-a a little bit more useful
    Toggle between going to the start of the first non-whitespace
    character and the start of the line.

--- .emacs.d/src/defuns-config.el
+++ .emacs.d/src/defuns-config.el
@@ -28,5 +28,17 @@ With arg N, insert N newlines."
     (shell-command-on-region b e
      "python -mjson.tool" (current-buffer) t)))
 
+(defun beginning-of-line-dwim ()
+  "Toggle between moving the point to the start of the first non-whitespace character and the start of the line."
+  (interactive)
+  (let ((start-position (point)))
+    ;; Move to the first non-whitespace character.
+    (back-to-indentation)
+    ;; If we haven't moved, go to the start of the line.
+    (when (= (point) start-position)
+      (move-beginning-of-line nil))))
+
+(global-set-key (kbd "C-a") 'beginning-of-line-dwim)
+
 (provide 'defuns-config)
 ;;; defuns-config.el ends here
