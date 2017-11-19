55a3764fe792c9c8a448e9251d9223c1f3aa3b49
Daniel Martín <mardani29@yahoo.es>
2017-11-19 01:10:12 +0100

    Use authentication info for Org2blog

--- .emacs.d/user-lisp/org2blog-config.el
+++ .emacs.d/user-lisp/org2blog-config.el
@@ -1,6 +1,18 @@
-(setq org2blog/wp-blog-alist
-      '(("cocoaengineering"
+;;; org2blog-config.el --- Org-Mode to Wordpress configuration.
+
+;;; Commentary:
+
+;;; Code:
+(use-package org2blog
+  :ensure t
+  :defer t
+  :config
+  (require 'org2blog-autoloads)
+  (setq org2blog/wp-confirm-post t)
+  (setq org2blog/wp-blog-alist
+      `(("cocoaengineering"
          :url "https://cocoaengineering.wordpress.com/xmlrpc.php"
-         :username "mardani29@gmail.com")))
+         :username ,(car (auth-source-user-and-password "cocoaengineering.wordpress.com"))
+         :password ,(cadr (auth-source-user-and-password "cocoaengineering.wordpress.com"))))))
 
 (provide 'org2blog-config)
