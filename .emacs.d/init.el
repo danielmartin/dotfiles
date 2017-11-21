548a9b331b7f0a73e0c949dd8dbcb2a80d3dc502
Daniel Martín <mardani29@yahoo.es>
2017-11-21 20:16:19 +0100

    Add git-undo
    Git-undo lets you select a region and then use M-x git-undo to revert
    changes in that region to the most recent Git historical version.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,8 @@
-
 ;; Generating This Document
 
 ;; Here's a convenience function that generates the files mentioned in
 ;; the previous section:
+
 
 (defun export-init-org ()
   "Generate init.el and init.htm from the current init.org file."
@@ -17,11 +17,13 @@
 
 ;;  I store the Emacs Lisp code that I write inside the user-lisp folder.
 
+
 (add-to-list 'load-path "~/.emacs.d/user-lisp")
 
 ;; Package Management
 
 ;; I use ELPA packages, mainly from the MELPA repository:
+
 
 (require 'package)
 (setq package-archives
@@ -30,13 +32,19 @@
         ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
+
+
 ;; Bootstrap use-package:
+
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+
+
 ;; Add an environment variable required by the PDF Tools package:
+
 
 (setenv "PKG_CONFIG_PATH" "/usr/local/Cellar/zlib/1.2.8/lib/pkgconfig:/usr/local/lib/pkgconfig:/opt/X11/lib/pkgconfig")
 
@@ -44,13 +52,20 @@
 
 ;; Start Emacs fully maximized:
 
+
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+
 
 ;; Some general functions that I've written.
 
+
 (require 'defuns-config)
 
+
+
 ;; And some general keybinding modifications.
+
 
 (require 'keybindings)
 
@@ -58,12 +73,16 @@
 
 ;; On a Mac, I use the Command key as Meta.
 
+
 (setq mac-option-modifier 'none)
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
 
+
+
 ;; GUI apps on macOS do not inherit $PATH from shell. This package solves
 ;; that.
+
 
 (use-package exec-path-from-shell
   :ensure t
@@ -81,6 +100,7 @@
 ;; use another for a special purpose (excellent Org-Mode support, a
 ;; presentation, etc.)
 
+
 (require 'appearance)
 
 ;; Ediff
@@ -88,12 +108,14 @@
 ;; I like Ediff's control panel to show in a separate frame, even on
 ;; graphical environments.
 
+
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
 ;; File Backup Management
 
 ;; Store Emacs backup files in their own directory, so as not to pollute
 ;; the current directory.
+
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq delete-old-versions -1)
@@ -106,9 +128,13 @@
 ;; I use Ido ("Interactive Do") as a simple way to switch between open
 ;; buffers.
 
+
 (ido-mode t)
 
+
+
 ;; Recentf is a mode for displaying recently open files.
+
 
 (recentf-mode 1)
 
@@ -116,13 +142,20 @@
 
 ;; I use linum mode, but only for programming modes.
 
+
 (add-hook 'prog-mode-hook 'linum-mode)
+
+
 
 ;; Format linum-mode output by appending a space at the end.
 
+
 (setq linum-format "%d ")
 
+
+
 ;; I also highlight the current line, only for programming modes.
+
 
 (add-hook 'prog-mode-hook 'hl-line-mode)
 
@@ -130,20 +163,30 @@
 
 ;; Show in which function or method the point is.
 
+
 (which-function-mode 1)
+
+
 
 ;; Show column numbers in the mode line.
 
+
 (column-number-mode 1)
+
+
 
 ;; I don't want some minor modes polluting my mode line. I use the
 ;; diminish package to avoid that.
+
 
 (use-package diminish
   :ensure t
   :defer t)
 
+
+
 ;; Show the current time.
+
 
 (use-package time
   :config
@@ -157,6 +200,7 @@
 
 ;; Show a project navigation tree using Neotree package.
 
+
 (use-package neotree
   :ensure t
   :defer t
@@ -166,6 +210,7 @@
 ;; Pairs
 
 ;; To ease working with pairs, I use the smartparens package.
+
 
 (use-package smartparens
   :ensure t
@@ -180,7 +225,10 @@
     (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
     (add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode))
 
+
+
 ;; Highlight parentheses pairs.
+
 
 (show-paren-mode 1)
 (set-face-background 'show-paren-match-face "#aaaaaa")
@@ -188,12 +236,18 @@
 :weight 'bold :underline nil :overline nil :slant
 'normal)
 
+
+
 ;; Close pairs automatically.
+
 
 (electric-pair-mode 1)
 
+
+
 ;; Rainbow-delimiters is a package which highlights delimiters such as
 ;; parentheses, brackets or braces according to their depth
+
 
 (use-package rainbow-delimiters
   :ensure t
@@ -213,11 +267,13 @@
 
 ;; Remove trailing whitespace before saving a file.
 
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Window Management
 
 ;; I use winner-mode to manage my windows with convenient undo/redo functions.
+
 
 (winner-mode 1)
 
@@ -227,11 +283,13 @@
 
 ;; I generally dislike tabs in my programs.
 
+
 (setq-default indent-tabs-mode nil)
 
 ;; C/C++/Objective-C/Objective-C++
 
 ;; For C languages, I use K&R style, with an indentation of 2 spaces.
+
 
 (use-package cc-mode
   :config
@@ -239,12 +297,18 @@
                            (c-set-style "k&r")
                            (setq c-basic-offset 2))))
 
+
+
 ;; As there's not a specific Emacs mode for this programming language,
 ;; for Objective-C++ files, use Objective-C mode.
 
+
 (add-to-list 'auto-mode-alist '("\\.mm$" . objc-mode))
 
+
+
 ;; Use a modern font lock mechanism.
+
 
 (use-package modern-cpp-font-lock
   :ensure t
@@ -252,7 +316,10 @@
   :config
   (add-hook 'c++-mode-hook #'modern-c++-font-lock-mode))
 
+
+
 ;; I use YouCompleteMe for C/C++ semantic autocompletion.
+
 
 (use-package ycmd
   :ensure t
@@ -266,7 +333,10 @@
   (set-variable 'ycmd-extra-conf-whitelist '("~/Projects/*"))
   (setq url-show-status nil))
 
+
+
 ;; I integrate ycm with company.
+
 
 (use-package company-ycmd
   :ensure t
@@ -277,7 +347,10 @@
   (setq company-idle-delay 0.1)
   (add-hook 'c++-mode-hook 'company-mode))
 
+
+
 ;; And also with flycheck.
+
 
 (use-package flycheck-ycmd
   :ensure t
@@ -291,6 +364,7 @@
 
 ;; Cider is the "de facto" package for working on Clojure projects.
 
+
 (use-package cider
   :ensure t
   :defer t)
@@ -300,6 +374,7 @@
 ;; Suggest.el is a nice package that helps you discover Elisp functions
 ;; that do what you want.
 
+
 (use-package suggest
   :ensure t
   :defer t)
@@ -307,6 +382,7 @@
 ;; JavaScript
 
 ;; Use js2-mode for JavaScript.
+
 
 (use-package js2-mode
   :ensure t
@@ -316,6 +392,7 @@
 
 ;; Use kotlin-mode for Kotlin development.
 
+
 (use-package kotlin-mode
   :ensure t
   :mode ("\\.kt\\'" . kotlin-mode))
@@ -324,6 +401,7 @@
 
 ;; Use Auctex with tex-site for an excellent LaTeX environment. Also,
 ;; enable RefTeX mode whenever a LaTeX document is open.
+
 
 (use-package tex-site
   :ensure auctex
@@ -335,11 +413,15 @@
 
 ;; I use markdown-mode to work on Markdown (.md) documents.
 
+
 (use-package markdown-mode
   :ensure t
   :mode ("\\.\\(m\\(ark\\)?down\\|md\\)$" . markdown-mode))
 
+
+
 ;; I want to fontify code blocks in Markdown:
+
 
 (setq markdown-fontify-code-blocks-natively t)
 
@@ -347,6 +429,7 @@
 
 ;; Emacs does not come with a mode for editing PHP mode. Just use
 ;; php-mode form the package repository.
+
 
 (use-package php-mode
   :ensure t
@@ -356,13 +439,17 @@
 
 ;; There are several packages for writing Python code. I use python.
 
+
 (use-package python
   :ensure t
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode))
 
+
+
 ;; Anaconda provides navigation documentation lookup and code completion
 ;; for Python:
+
 
 (use-package anaconda-mode
   :ensure t
@@ -371,7 +458,10 @@
   (add-hook 'python-mode-hook 'anaconda-mode)
   (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
 
+
+
 ;; Integrate Anaconda with company:
+
 
 (use-package company-anaconda
   :ensure t
@@ -379,7 +469,10 @@
   :config
   (add-to-list 'company-backends 'company-anaconda))
 
+
+
 ;; Format Python code according to PEP8:
+
 
 (use-package py-autopep8
   :ensure t
@@ -393,11 +486,13 @@
 
 ;; For linting Shell scripts, I integrate Shellcheck with Flycheck.
 
+
 (add-hook 'sh-mode-hook 'flycheck-mode)
 
 ;; Swift
 
 ;; I use swift-mode for Swift code.
+
 
 (use-package swift-mode
   :ensure t
@@ -411,6 +506,7 @@
 ;; TableGen is an abstract IDL used by LLVM and related projects to
 ;; generate code automatically.
 
+
 (add-to-list 'load-path "~/Projects/llvm/utils/emacs")
 (require 'tablegen-mode)
 
@@ -418,6 +514,7 @@
 
 ;; Autocompletion is very important for programming languages and natural
 ;; languages. I use company for that.
+
 
 (use-package company
   :ensure t
@@ -431,6 +528,7 @@
 
 ;; CMake is a meta-build system that is commonly used in C++ projects.
 
+
 (use-package cmake-mode
   :ensure t
   :defer t)
@@ -440,6 +538,7 @@
 ;; Code formatting tools make smarter decisions than typical Emacs
 ;; indenters, specially for complex languages like C++. As yet, I use
 ;; clang-format for C++ and related languages.
+
 
 (use-package clang-format
   :ensure t
@@ -453,12 +552,16 @@
 ;; Sourcetrail is a great indexer to make sense of a big C/C++/Java
 ;; project.
 
+
 (use-package sourcetrail
   :ensure t
   :bind
   ("C-c s" . sourcetrail-send-location))
 
+
+
 ;; For quick navigation inside a source file, I use ace-jump-mode.
+
 
 (use-package ace-jump-mode
   :ensure t
@@ -466,8 +569,11 @@
   :init
   (global-set-key (kbd "C-c SPC") 'ace-jump-mode))
 
+
+
 ;; Typically, I want to navigate quickly over the instances of a
 ;; particular symbol in a source file.
+
 
 (use-package highlight-symbol
   :ensure t
@@ -487,6 +593,7 @@
 
 ;; Use expand-region to increase the selected region by semantic units.
 
+
 (use-package expand-region
   :ensure t
   :defer t
@@ -499,6 +606,7 @@
 ;; which is a nice abstraction over several debuggers for programming
 ;; languages.
 
+
 (use-package realgud
   :ensure t
   :defer t)
@@ -506,6 +614,7 @@
 ;; Documentation
 
 ;; For showing inline documentation for Emacs Lisp functions, I use eldoc.
+
 
 (use-package eldoc
   :ensure t
@@ -516,8 +625,11 @@
   (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
   (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode))
 
+
+
 ;; In general, I use Dash docsets for any programming language. For now,
 ;; dash-at-point only works for C++ files.
+
 
 (use-package dash-at-point
   :ensure t
@@ -529,6 +641,7 @@
 ;; Magit
 
 ;; Magit is the best Git porcelain I've ever used.
+
 
 (use-package magit
   :ensure t
@@ -542,6 +655,7 @@
 ;; MagitHub extends Magit with functions to work on GitHub repositories
 ;; (show open issues, PRs, etc.).
 
+
 (use-package magithub
   :after magit
   :ensure t
@@ -553,13 +667,24 @@
 ;; git-timemachine is a package that intuitively shows previous versions
 ;; of a particular file from a Git repository.
 
+
 (use-package git-timemachine
   :ensure t
   :defer t)
 
+;; Git Undo
+
+
+;; Git-undo lets you select a region and revert changes in that region to
+;; the most recent Git historical version.
+
+
+(add-to-list 'load-path "~/.emacs.d/user-lisp/git-undo")
+
 ;; Browse at Remote
 
 ;; This package browses target pages at GitHub/Bitbucket.
+
 
 (use-package browse-at-remote
   :ensure t
@@ -569,6 +694,7 @@
 ;; Helm
 
 ;; Helm is a great incremental completion and selection narrowing framework.
+
 
 (use-package helm
   :ensure t
@@ -594,6 +720,7 @@
 
 ;; Better help system.
 
+
 (use-package helpful
   :ensure t
   :defer t)
@@ -601,6 +728,7 @@
 ;; Natural Languages
 
 ;; For checking spelling and grammar, I use an external Java tool: Language-tool.
+
 
 (use-package langtool
   :ensure t
@@ -612,6 +740,7 @@
 
 ;; Org-Mode configuration is handled in a separate file.
 
+
 (require 'org-mode-config)
 
 ;; PDF Tools
@@ -619,6 +748,7 @@
 ;; I want a nice way to work on PDF documents graphically.
 
 ;; Install with `brew install pdf-tools`.
+
 
 (use-package pdf-tools
   :ensure t
@@ -628,7 +758,10 @@
   (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo")
   (pdf-tools-install))
 
+
+
 ;; pdf-linter will "lint" a PDF document using PDFBox Preflight app.
+
 
 (require 'pdf-linter)
 (setq pdf-linter-jar "$HOME/PDFBox/preflight-app-2.0.7.jar")
@@ -638,6 +771,7 @@
 ;; Programs are usually organized in projects, being a Git repo a natural
 ;; way to define one. I use Projectile to work on projects.
 
+
 (use-package projectile
   :ensure t
   :config
@@ -645,7 +779,10 @@
   (setq projectile-completion-system 'helm)
   (helm-projectile-on))
 
+
+
 ;; I also integrate Projectile with Helm.
+
 
 (use-package helm-projectile
   :ensure t
@@ -655,6 +792,7 @@
 
 ;; Pandoc is a tool to convert between almost every document format.
 
+
 (use-package pandoc-mode
   :ensure t
   :defer t)
@@ -662,6 +800,7 @@
 ;; REST
 
 ;; For making REST calls from Emacs, I use the convenient restclient package.
+
 
 (use-package restclient
   :ensure t
@@ -673,6 +812,7 @@
 ;; way of grepping a project, I use Silver Searcher (ag) and integrate it
 ;; with Helm.
 
+
 (use-package helm-ag
   :ensure t
   :bind ("C-c p A" . helm-do-ag-project-root))
@@ -681,13 +821,17 @@
 
 ;; I use yasnippet for managing text snippets.
 
+
 (use-package yasnippet
   :ensure t
   :defer t
   :diminish yas-minor-mode
   :init (yas-global-mode 1))
 
+
+
 ;; Manage abbreviations with abbrev-mode.
+
 
 (use-package abbrev
   :diminish abbrev-mode)
@@ -696,12 +840,16 @@
 
 ;; I use flycheck for "on the fly" syntax checking.
 
+
 (use-package flycheck
   :ensure t
   :defer t
   :init (global-flycheck-mode))
 
+
+
 ;; Don't check documentation by default (good thing for small throwaway scripts).
+
 
 (setq flycheck-checkers (--remove (eq it 'emacs-lisp-checkdoc) flycheck-checkers))
 
@@ -709,6 +857,7 @@
 
 ;; For a more intuitive undo/redo management, I use undo-tree instead of
 ;; the default undo/redo system.
+
 
 (use-package undo-tree
   :ensure t
@@ -731,16 +880,21 @@
 ;; I've created a simple major mode that toggles between showing raw and
 ;; detailed information about a X.509 certificate.
 
+
 (require 'x509-certificate-mode)
 
 ;; Xcode Projects
 
 ;; I've created a package for working on Xcode projects.
 
+
 (add-to-list 'load-path "~/.emacs.d/user-lisp/pbxproj-mode")
 (require 'pbxproj-mode)
 
+
+
 ;; I've also added on-the-fly syntax checking capabilities.
+
 
 (add-to-list 'load-path "~/.emacs.d/user-lisp/flycheck-pbxproj")
 (require 'flycheck-pbxproj)
