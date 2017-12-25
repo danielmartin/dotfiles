a65c3108ba5fa8e7544b9ab4d3ee22adeb2f84d3
Daniel Martín <mardani29@yahoo.es>
2017-12-25 15:32:32 +0100

    Diff binary files using Hexl

--- .emacs.d/user-lisp/defuns-config.el
+++ .emacs.d/user-lisp/defuns-config.el
@@ -54,5 +54,46 @@ With arg N, insert N newlines."
 
 (global-set-key (kbd "M-g c") 'dwim-goto-char)
 
+(defvar ediff-do-hexl-diff t "Variable used to store trigger for doing diff in hexl-mode")
+(defadvice ediff-files-internal (around
+ediff-files-internal-for-binary-files activate)
+  "Catch the condition when the binary files differ the reason
+for catching the error out here (when re-thrown from the inner
+advice) is to let the stack continue to unwind before we start
+the new diff otherwise some code in the middle of the stack
+expects some output that isn't there and triggers an error"
+  (let ((file-A (ad-get-arg 0))
+        (file-B (ad-get-arg 1))
+        ediff-do-hexl-diff)
+    (condition-case err
+        (progn
+          ad-do-it)
+      (error
+       (if ediff-do-hexl-diff
+           (let ((buf-A (find-file-noselect file-A))
+                 (buf-B (find-file-noselect file-B)))
+             (with-current-buffer buf-A
+               (hexl-mode 1))
+             (with-current-buffer buf-B
+               (hexl-mode 1))
+             (ediff-buffers buf-A buf-B))
+         (error (error-message-string err)))))))
+
+(defadvice ediff-setup-diff-regions (around ediff-setup-diff-regions-for-binary-files activate)
+  "When binary files differ, set the variable "
+  (condition-case err
+      (progn
+        ad-do-it)
+    (error
+     (setq ediff-do-hexl-diff
+           (and (string-match-p "^Errors in diff output.  Diff output is in.*"
+                                (error-message-string err))
+                (string-match-p "^\\(Binary \\)?[fF]iles .* and .* differ"
+                                (buffer-substring-no-properties
+                                 (line-beginning-position)
+                                 (line-end-position)))
+                (y-or-n-p "The binary files differ, look at the differences in hexl-mode? ")))
+     (error (error-message-string err)))))
+
 (provide 'defuns-config)
 ;;; defuns-config.el ends here
