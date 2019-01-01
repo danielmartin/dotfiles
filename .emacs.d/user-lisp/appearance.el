446872b23fdce27c4ff13d00f0c5135c525a6ad6
Daniel Martín <mardani29@yahoo.es>
2019-01-01 02:02:10 +0100

    Add alternative themes

--- .emacs.d/user-lisp/appearance.el
+++ .emacs.d/user-lisp/appearance.el
@@ -13,6 +13,14 @@
     :config
     (setf custom-safe-themes t)
     (color-theme-sanityinc-tomorrow-night)
+    (setq moody-slant-function #'moody-slant-apple-rgb)
+    (let ((line (face-attribute 'mode-line :underline)))
+      (set-face-attribute 'mode-line          nil :overline   line)
+      (set-face-attribute 'mode-line-inactive nil :overline   line)
+      (set-face-attribute 'mode-line-inactive nil :underline  line)
+      (set-face-attribute 'mode-line          nil :box        nil)
+      (set-face-attribute 'mode-line-inactive nil :box        nil)
+      (set-face-attribute 'mode-line-inactive nil :background "#161424"))
     (custom-set-faces
      '(lsp-face-highlight-textual ((t :background "#464752")))
      '(cursor ((t :background "#eebb28"))))))
@@ -24,7 +32,42 @@
     :defer t)
   (load-theme 'leuven t))
 
+(defun use-solarized-theme ()
+  (interactive)
+  (use-package solarized-theme
+    :config
+    (load-theme 'solarized-light t)
+    (let ((line (face-attribute 'mode-line :underline)))
+      (set-face-attribute 'mode-line          nil :overline   line)
+      (set-face-attribute 'mode-line-inactive nil :overline   line)
+      (set-face-attribute 'mode-line-inactive nil :underline  line)
+      (set-face-attribute 'mode-line          nil :box        nil)
+      (set-face-attribute 'mode-line-inactive nil :box        nil)
+      (set-face-attribute 'mode-line-inactive nil :background "#f9f2d9"))))
+
+(defun use-doom-theme ()
+  (interactive)
+  (use-package doom-themes
+    :ensure t
+    :config
+    ;; Global settings (defaults)
+    (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+          doom-themes-enable-italic t) ; if nil, italics is universally disabled
+    ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+    ;; may have their own settings.
+    (load-theme 'doom-one t)
+    ;; Enable flashing mode-line on errors
+    (doom-themes-visual-bell-config)
+    ;; Enable custom neotree theme (all-the-icons must be installed!)
+    (doom-themes-neotree-config)
+    ;; or for treemacs users
+    (doom-themes-treemacs-config)
+    ;; Corrects (and improves) org-mode's native fontification.
+    (doom-themes-org-config)))
+
 (use-default-theme)
+;; (use-solarized-theme)
+;; (use-doom-theme)
 
 ;; Ditch the splash screen
 (setq inhibit-splash-screen t)
