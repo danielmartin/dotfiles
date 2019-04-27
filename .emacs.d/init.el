c72315450eb2756a7c4e258351b27339fa9c8eed
Daniel Martín <mardani29@yahoo.es>
2019-04-27 14:54:54 +0200

    Fix unclosed parenthesis

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,7 +28,7 @@
 (require 'package)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/"))
+        ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 
