368b3b00ecf554e53c11fde65612222315df87e6
Daniel Martín <mardani29@yahoo.es>
2019-05-01 01:16:36 +0200

    Update PDF tools package configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -48,7 +48,7 @@
 ;; Add an environment variable required by the PDF Tools package:
 
 
-(setenv "PKG_CONFIG_PATH" "/usr/local/Cellar/zlib/1.2.8/lib/pkgconfig:/usr/local/lib/pkgconfig:/opt/X11/lib/pkgconfig")
+(setenv "PKG_CONFIG_PATH" "/usr/local/lib/pkgconfig:/usr/local/Cellar/libffi/3.2.1/lib/pkgconfig")
 
 ;; General Customizations
 
