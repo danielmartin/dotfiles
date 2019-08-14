ef51aa7f254e2c6d65a9c62a6c60d35240afcf3a
Daniel Martín <mardani29@yahoo.es>
2019-08-15 00:20:42 +0200

    Configure markdown-mode to place reference links at the end
    This is what the Markdown projects I work on usually do.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -581,7 +581,8 @@
 (use-package markdown-mode
   :ensure t
   :mode (("\\`README\\.md\\'" . gfm-mode))
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "multimarkdown")
+  :config (setq markdown-reference-location 'end))
 
 
 
