154da613be1067406fbad3e15248d35ab0f97cc9
Daniel Martín <mardani29@yahoo.es>
2019-12-07 15:18:36 +0100

    Add a custom function to generate a histogram of buffers
    * .emacs.d/user-lisp/defuns-config.el (dm/buffer-mode-histogram): Add
    a function to generate a histogram of open buffers in Emacs.

--- .emacs.d/user-lisp/defuns-config.el
+++ .emacs.d/user-lisp/defuns-config.el
@@ -204,5 +204,35 @@ Time is formatted in hours, minutes, and seconds."
 (require 'compile)
 (define-key compilation-mode-map (kbd "c") #'dm/print-compile-time)
 
+;;; http://blogs.fluidinfo.com/terry/2011/11/10/emacs-buffer-mode-histogram/
+(defun dm/buffer-mode-histogram ()
+  "Display a histogram of emacs buffer modes."
+  (interactive)
+  (let* ((totals ())
+         (buffers (buffer-list()))
+         (total-buffers (length buffers))
+         (ht (make-hash-table :test 'equal)))
+    (save-excursion
+      (dolist (buffer buffers)
+        (set-buffer buffer)
+        (let
+            ((mode-name (symbol-name major-mode)))
+          (puthash mode-name (1+ (gethash mode-name ht 0)) ht))))
+    (maphash (lambda (key value)
+               (setq totals (cons (list key value) totals)))
+             ht)
+    (setq totals (sort totals (lambda (x y) (> (cadr x) (cadr y)))))
+    (with-output-to-temp-buffer "Buffer mode histogram"
+      (princ (format "%d buffers open, in %d distinct modes\n\n"
+                      total-buffers (length totals)))
+      (dolist (item totals)
+        (let
+            ((key (car item))
+             (count (cadr item)))
+          (if (equal (substring key -5) "-mode")
+              (setq key (substring key 0 -5)))
+          (princ (format "%2d %20s %s\n" count key
+                         (make-string count ?+))))))))
+
 (provide 'defuns-config)
 ;;; defuns-config.el ends here
