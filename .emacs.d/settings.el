73c12385420c3b74844b2d6a843724c4c61943fd
Daniel Martín <mardani29@yahoo.es>
2020-01-05 20:48:39 +0100

    Add settings.el with custom faces

---
+++ .emacs.d/settings.el
@@ -0,0 +1,7 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "SF Mono"))))
+ '(cursor ((t (:background "red")))))
