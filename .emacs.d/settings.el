aad36cbf4e36b360738ac47e80f5abd656c700a3
Daniel Martín <mardani29@yahoo.es>
2020-01-26 17:33:01 +0100

    Change default background color
    Make the background color darker gray instead of black. Also, decrease
    the font size a little bit.

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -3,5 +3,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "SF Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "gray15" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "SF Mono"))))
  '(cursor ((t (:background "red")))))
