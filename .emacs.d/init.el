21f7c33440bcbaa0935f1aa2a0389b5f928cc3f1
Daniel Martín <mardani29@yahoo.es>
2020-08-13 01:17:58 +0200

    Do not call (package-initialize) in Emacs versions >= 27
    It's not needed anymore.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -53,7 +53,8 @@
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
+(when (< emacs-major-version 27)
+  (package-initialize))
 
 
 
